e221316c0d3da2582f17c424543d4607d1db177c
Muhammad Fahim Faysal <76892541+fahim484@users.noreply.github.com>
2026-03-02 10:24:32 +0000

    Add JavaScript data types and examples for better understanding

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,42 @@
+"use strict"; // <-- Syntax, and treat all JavaScript Code as a newer version of JavaScript Code.
+
+// alert("hello") // [alert()] This will work on Browser, not here(Node.js)
+// alert(3 + 3) // Arithmetic operation
+
+// console.log(3 + 3) console.log("Fahim") // Do Not use
+
+console.log(3
+     + 
+     3) // Do Not use // code readability should be high(not use)
+
+console.log(3 + 3) // (use this)
+
+console.log("Fahim") // (use this)
+
+
+//JavaScript (JS) DataTypes:
+let name = "Fahim" // String
+let age = 18 // Number
+let isLoggedIn = false // Boolean
+
+
+/* Primitive DataTypes : 
+1. number => 2 to power 53(bigint),
+2. bigint => very big number(especially trading, stock market, reddit, etc.))
+2. string => "" ("" or ''),
+3. boolean => true/false (user loggedIn yes or no, credit card details available yes or no, server response yes or no, etc.),
+4. null => standalone value (empty value, like let price = null(it's not undefined, it's only empty(ex: server send null value as a temperature if server not working. ))),
+5. undefined => let state; (declared a variable but not assigned any value.)
+6. symbol (unique(React.js))
+*/
+
+
+
+//JavaScript (JS) Object(type):
+
+console.log(typeof "Fahim"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object // Interview Question
+console.log(typeof undefined); // undefined
+
+
