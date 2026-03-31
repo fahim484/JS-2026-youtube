a753e8e1c6d37c829ac720c50871272e737bc46a
Muhammad Fahim Faysal <76892541+fahim484@users.noreply.github.com>
2026-03-31 11:00:44 +0000

    Add examples and explanations for toPrecision() and Math methods, including abs, round, ceil, floor, min, max, and random

--- 01_basics/11_nums_and_math.js
+++ 01_basics/11_nums_and_math.js
@@ -19,3 +19,53 @@ console.log(x.toExponential(6)); // 9.656000e+0
 
 // 03. (The toFixed() Method): toFixed() returns a string, with the number written with a specified number of decimals:
 console.log(balance.toFixed(2)); // 100.00
+// Note: toFixed(2) is perfect for working with money.
+
+// 04. (The toPrecision() Method): toPrecision() returns a string, with a number written with a specified length:'
+const otherNumber = 23.8966
+console.log(otherNumber.toPrecision(3)); // 23.9
+const anotherNumber = 123.8966
+console.log(anotherNumber.toPrecision(3)); // 124
+console.log(anotherNumber.toPrecision(4)); // 123.9
+const newNumber = 1123.8966
+console.log(newNumber.toPrecision(3)); // 1.12e+3
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString()); // 1,000,000 // Default U.S Standard
+console.log(hundreds.toLocaleString('en-IN')); // 10,00,000 // Indian Standard
+console.log(hundreds.toLocaleString('bn-BD')); // ১০,০০,০০০ // Bangladeshi Standard
+console.log(hundreds.toLocaleString('bn-BD', {
+  style: 'currency',
+  currency: 'BDT',
+  minimumFractionDigits: 2
+})); // ১০,০০,০০০.০০৳
+
+
+
+// +++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++
+
+console.log(Math); // Object [Math] {}
+console.log(Math.abs(-4)); // 4 (+4) // Negative to Positive
+console.log(Math.abs(4)); // 4 (+4) // Positive to Positive
+console.log(Math.round(4.3)); // 4
+console.log(Math.round(4.4)); // 4
+console.log(Math.round(4.5)); // 5
+console.log(Math.round(4.6)); // 5
+console.log(Math.ceil(4.2)); // 5
+console.log(Math.ceil(4.0)); // 4
+console.log(Math.floor(4.0)); // 4
+console.log(Math.floor(4.2)); // 4
+console.log(Math.floor(4.9)); // 4
+console.log(Math.min(4, 3, 6, 8, 2, 9 , 15, 7)); // 2
+console.log(Math.max(4, 3, 6, 8, 2, 9 , 15, 7)); // 15
+
+console.log(Math.random()); // Always looping value between 0 to 1(0-1) (Decimal)
+console.log((Math.random() * 10) + 1); // 
+console.log(Math.floor(Math.random() * 10) + 1); // Lowest Value for "floor"
+
+
+// Tips & Tricks:
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
