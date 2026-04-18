2457cbc7fec9263da1882f8ecf762985350a799f
Muhammad Fahim Faysal <76892541+fahim484@users.noreply.github.com>
2026-04-18 10:35:46 +0000

    Add examples for flattening arrays and converting various data types to arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -18,3 +18,21 @@ console.log(all_new_heroes_merged); // [ 'Thor', 'Iron man', 'Spider man', 'Supe
 
 // 03. 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) // SpreadOut
+console.log(real_another_array); // [1, 2, 3, 4, 5, 6, 7, 6, 7, 4, 5]
+
+
+// 04. Data Scraping(NodeList, Object, String --> Array(Loop)) (Convert to Array)
+console.log(Array.isArray("Fahim")); // false
+console.log(Array.from("Fahim")); // [ 'F', 'a', 'h', 'i', 'm' ]
+console.log(Array.from({name: "Fahim"})); // [] // Interview Question
+
+
+
+// 05.
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]
+
